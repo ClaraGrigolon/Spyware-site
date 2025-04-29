9712411d473db7170e82729d719c06711b4447a5
ClaraGrigolon <claragrigolon@gmail.com>
2025-04-29 17:20:34 -0300

    Add files via upload

--- src/assets/components/Content.jsx
+++ src/assets/components/Content.jsx
@@ -12,16 +12,16 @@ function Content(){
                 <p>Um jeito fácil de tentar identificar se seu computador tem spyware é usando o Gerenciador de Tarefas do Windows. Para isso, aperte Ctrl + Shift + Esc para abrir a janela do Gerenciador. Olhe os programas que estão rodando e veja se há algo estranho ou que você não reconhece. Se encontrar algo suspeito, pesquise o nome na internet. Além disso, você pode usar um antivírus confiável para fazer uma varredura completa no seu computador.</p> <br />
 
                 <h2>Como remover?</h2>
-                <p>Para remover spyware do seu computador, comece desconectando-o da internet para evitar que o spyware envie dados. Em seguida, inicie o sistema no Modo de Segurança para impedir a execução do spyware. Utilize um antivírus ou antimalware, como o Malwarebytes, para escanear e remover o spyware. Verifique também se há programas suspeitos e desinstale-os, além de limpar os dados de navegação no seu navegador. É importante também verificar os processos em execução e atualizar o sistema para corrigir falhas de segurança. Após isso, reinicie o computador e faça uma nova verificação para garantir que o spyware foi completamente removido.</p> <br />
+                <p>Para remover spyware do seu computador, comece desconectando-o da internet para evitar que ele envie dados. Em seguida, inicie o sistema no Modo de Segurança para impedir a execução do software espião. Utilize um antivírus ou antimalware, como o Malwarebytes, para escanear e remover o spyware. Verifique também se há programas suspeitos e desinstale-os, além de limpar os dados de navegação no seu navegador. É importante também verificar os processos em execução e atualizar o sistema para corrigir falhas de segurança. Após isso, reinicie o computador e faça uma nova verificação para garantir que o spyware foi completamente removido.</p> <br />
 
                 <h2>5 formas de me proteger contra Spyware</h2>
-                <li>Instale um bom antivírus: Um antivírus confiável ajuda a detectar e bloquear spywares antes que eles infectem o seu sistema. Mantenha-o sempre atualizado.</li>
+                <li>Instale um bom antivírus: Um antivírus confiável ajuda a detectar e bloquear spywares antes que eles infectem o seu sistema;</li>
+                
+                <li>Mantenha o sistema e programas atualizados: Instale regularmente atualizações de segurança para o sistema operacional e softwares. Muitas infecções acontecem por falhas de segurança não corrigidas;</li>
 
-                <li>Mantenha o sistema e programas atualizados: Instale regularmente atualizações de segurança para o sistema operacional e softwares. Muitas infecções acontecem por falhas de segurança não corrigidas.</li>
+                <li>Evite clicar em links desconhecidos: Não clique em links ou baixe arquivos de fontes não confiáveis, como e-mails ou sites desconhecidos;</li>
 
-                <li>Evite clicar em links desconhecidos: Não clique em links ou baixe arquivos de fontes não confiáveis, como e-mails ou sites desconhecidos.</li>
-
-                <li>Não baixe programas de fontes não oficiais: Evite fazer download de softwares de sites não confiáveis ou de fontes desconhecidas, pois muitos deles podem conter spywares.</li>
+                <li>Não baixe programas de fontes não oficiais: Evite fazer download de softwares de sites não confiáveis ou de fontes desconhecidas, pois muitos deles podem conter spywares;</li>
 
                 <li>Use uma rede segura: Evite usar redes Wi-Fi públicas para transações ou navegação sensível, já que elas podem ser alvos fáceis para ataques.</li> <br />
 
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import Logo from '../img/spy-simbol.png';
 
 function Header(){
     return(
         <>
           <header>
+            <img src={Logo} alt="Logo" />
             <h1>Spyware</h1>
           </header>
         </>
